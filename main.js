228eb50468d0d29c9c773d47b42dfee8f8fa1bd9
Babajide Tomoshegbo <babajide234@gmail.com>
2023-02-07 04:16:51 +0100

    responsiveness and touch

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ function nextSlide() {
   dots[currentSlide].classList.add('active-dot');
 }
 
-var nextButton = document.querySelector('#next-button');
+// var nextButton = document.querySelector('#next-button');
 
-nextButton.addEventListener('click', function() {
-  nextSlide();
-});
+// nextButton.addEventListener('click', function() {
+//   nextSlide();
+// });
 
 dots.forEach(function(dot, index) {
   dot.addEventListener('click', function() {
@@ -27,6 +27,26 @@ dots.forEach(function(dot, index) {
     slides[currentSlide].classList.add('active-slide');
     dots[currentSlide].classList.add('active-dot');
   });
+});
+
+
+var startX = 0;
+var endX = 0;
+
+slides[0].addEventListener('touchstart', function(e) {
+  startX = e.touches[0].pageX;
+});
+slides[0].addEventListener('touchend', function(e) {
+  endX = e.changedTouches[0].pageX;
+  if (startX > endX + 50) {
+    nextSlide();
+  } else if (startX < endX - 50) {
+    slides[currentSlide].classList.remove('active-slide');
+    dots[currentSlide].classList.remove('active-dot');
+    currentSlide = (currentSlide + slides.length - 1) % slides.length;
+    slides[currentSlide].classList.add('active-slide');
+    dots[currentSlide].classList.add('active-dot');
+  }
 });
 
 slides[currentSlide].classList.add('active-slide');
