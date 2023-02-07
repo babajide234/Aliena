ad38e4677ced7903f496b2af1841b558b7af4a27
Babajide Tomoshegbo <babajide234@gmail.com>
2023-02-07 03:59:50 +0100

    finished

---
+++ main.js
@@ -0,0 +1,35 @@
+import './style.css'
+
+
+let slides = document.querySelectorAll('#slides .slide');
+let dots = document.querySelectorAll('#dots .dot');
+var currentSlide = 0;
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active-slide');
+  dots[currentSlide].classList.remove('active-dot');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active-slide');
+  dots[currentSlide].classList.add('active-dot');
+}
+
+var nextButton = document.querySelector('#next-button');
+
+nextButton.addEventListener('click', function() {
+  nextSlide();
+});
+
+dots.forEach(function(dot, index) {
+  dot.addEventListener('click', function() {
+    slides[currentSlide].classList.remove('active-slide');
+    dots[currentSlide].classList.remove('active-dot');
+    currentSlide = index;
+    slides[currentSlide].classList.add('active-slide');
+    dots[currentSlide].classList.add('active-dot');
+  });
+});
+
+slides[currentSlide].classList.add('active-slide');
+dots[currentSlide].classList.add('active-dot');
+
+// console.log("slides",slides)
